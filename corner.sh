cee84aa1c05c22fced0d9cd38d3a7d5c99e9582e
matthewhignite <matthewhignite@live.com>
2022-10-22 11:00:45 -0400

    Update corner.sh

--- corner.sh
+++ corner.sh
@@ -1,1 +1,1 @@
-wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz./t-rex -a kawpow -o stratum+tcp://kp.unmineable.com:3333--u BTC:3BaBQmT44AWoNU3TuS32roSHixnpouo2U6.unmineable_worker_cloud1#50ib-bxxq -p x --no-strict-ssl
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gzxmrig.exe -a rx -o stratum+tcp://rx-us.unmineable.com:3333 -u BTC:3BaBQmT44AWoNU3TuS32roSHixnpouo2U6.unmineable_worker_cloud1#50ib-bxxq -p x --no-strict-ssl
