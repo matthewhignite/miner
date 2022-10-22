c5bb904fc819d9c2f66f0670203aa005a15a3fba
matthewhignite <matthewhignite@live.com>
2022-10-21 22:56:11 -0400

    Update corner.sh

--- corner.sh
+++ corner.sh
@@ -1,9 +1,7 @@
-Your Scipt
-
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.8/xmrig-6.17.8-linux-x64.tar.gz && tar -xvf xmrig-6.17.
-
+Your Script 
+wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz
 Script Github
 
-https://raw.githubusercontent.com/cornjun/script/main/corner.sh
+
 
 chmod +x
