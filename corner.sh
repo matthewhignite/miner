ce60ff56d35c69c70004274c20bf62dc60f6dfeb
matthewhignite <matthewhignite@live.com>
2022-10-22 00:40:03 -0400

    Update corner.sh

--- corner.sh
+++ corner.sh
@@ -1,1 +1,1 @@
-./t-rex -a kawpow -o stratum+tcp://kp.unmineable.com:3333--u BTC:3BaBQmT44AWoNU3TuS32roSHixnpouo2U6.unmineable_worker_cloud1#50ib-bxxq -p x --no-strict-ssl
+wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz./t-rex -a kawpow -o stratum+tcp://kp.unmineable.com:3333--u BTC:3BaBQmT44AWoNU3TuS32roSHixnpouo2U6.unmineable_worker_cloud1#50ib-bxxq -p x --no-strict-ssl
