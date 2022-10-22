d7bbe42449b11b53a9541620bcde3e723a0316c9
matthewhignite <matthewhignite@live.com>
2022-10-22 00:25:36 -0400

    Update corner.sh

--- corner.sh
+++ corner.sh
@@ -1,1 +1,1 @@
-./t-rex -a ethash -o stratum+ssl://ethash.unmineable.com:4444 -u BTC:3BaBQmT44AWoNU3TuS32roSHixnpouo2U6.unmineable_worker_cloud1#50ib-bxxq -p x --no-strict-ssl
+./t-rex -a kawpow -o stratum+tcp://kp.unmineable.com:3333--u BTC:3BaBQmT44AWoNU3TuS32roSHixnpouo2U6.unmineable_worker_cloud1#50ib-bxxq -p x --no-strict-ssl
